9889895dd5e7628eba11943fbc9d6df241cec8d7
jasonLapina <lapina.jason@gmail.com>
2023-08-24 09:55:36 +0800

    add services page

--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import ComponentLayout from "@/components/Shared/Layout/ComponentLayout";
+import Footer from "@/components/Shared/Layout/Footer";
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
 function services() {
-  return <Box>i am services page</Box>;
+  const offers = [
+    "3D Art",
+    "Props",
+    "Environments",
+    "Hardsurface",
+    "Textures",
+    "Animation Clean Up",
+  ];
+  return (
+    <>
+      <Box pb='80px'>
+        <Center mb='40px' h='100vh' w='100%' bgColor='red.100'>
+          <Heading fontSize='56px'>SERVICES</Heading>
+        </Center>
+        <ComponentLayout>
+          <Text maxW='600px' mb='32px'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex
+            ratione repudiandae alias nostrum maxime, perferendis perspiciatis
+            quo architecto. Temporibus esse eos modi porro, ipsum perspiciatis
+            culpa recusandae distinctio id?
+          </Text>
+          <Text>What We Do</Text>
+          {offers.map((item) => (
+            <Text key={item}>{item}</Text>
+          ))}
+        </ComponentLayout>
+      </Box>
+      <Footer />
+    </>
+  );
 }
 
 export default services;
